8f552ce10f7d066a1b7341f53b76174b825caec3
David Buckley <david@davidbuckley.ca>
2022-07-17 19:35:56 -0400

    bump version

--- flask_discord_interactions/__init__.py
+++ flask_discord_interactions/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from flask_discord_interactions.discord import Discord
 from flask_discord_interactions.discord_types import (
@@ -25,34 +25,7 @@ from flask_discord_interactions.interactions import (
 # TODO: There should be a mechanism to opt-out of automatically init_commands-ing
 # TODO: How do modals work?
 
-# TODO: Message Components
-# I think the best way forward here is all around concepts of "registering" handlers somehow within the functions themselves so they can dynamically include/exclude components with custom handling depending on whatever they want. Static handlers defined at start-time can't do that.
-# This can coincide with making responses easier, as the response object could be what you register handlers on.
-"""
-@discord.command("slash-example")
-def chat_command(interaction: interactions.ChatInteraction) -> types.InteractionResponse:
-    @components.Button(...)
-    def button(interaction) -> InteractionResponse:
-      ...
-
-    OR
-
-    button = components.Button(...)
-    button.handler = handle_button
-
-    return types.InteractionResponse(
-        type=types.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data=types.InteractionCallbackDataMessage(
-            content="Hello, World!"
-            components=[ActionRow(components=[button])]
-        )
-    )
-chat_command.description = "Say Hello via a slash command"
-"""
-
-# Before sending the response back to Discord, we scan the `components` for the handlers so we can route future requests to them.
-#
 # TODO: Better ergonomics on response
 # 1. A command should be able to just return a str to make the simple case easy
 # 2. Decide how to handle deferred-type responses
-# 3.
+# 3. More helpers?
